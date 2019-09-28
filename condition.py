39db1ecc4424d184b93144aab673d90dfc8a4c75
Celestin NIYONSABA <niyoceles3@gmal.com>
2019-09-28 18:04:08 +0200

    basic of python - basic operation - guessing game

---
+++ condition.py
@@ -0,0 +1,18 @@
+is_hot = True
+is_cold = True
+
+if is_hot:
+    print("it is hot, you can wake up")
+elif is_cold:
+    print("is cold don't wake up")
+else:
+    print("wahh uuuuu!")
+
+print('Better day')
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i= i+3
+print("Done")
---
+++ first.py
@@ -0,0 +1,31 @@
+print("Hello World")
+print("this the the first application in Python")
+# print another
+print('Not print comment')
+
+print("yves", (25+24)/2)
+
+# variables
+expenses = 3000
+incomes = 10000
+
+print('profit:', incomes-expenses)
+print('the profit %d Rwf.', expenses)
+print('''
+Kigali ngali
+Kigali ville
+''')
+print('how are old are you')
+age = input()
+name = input('What is your name ')
+print('how far are you')
+answer = input()
+print("my name "+name, "so, I have", age, "old and you fell", answer)
+
+#Calculate age
+
+birfh_date = input('What is your birthday ')
+years= 2019- int(birfh_date)
+
+print('I have ', years)
+
